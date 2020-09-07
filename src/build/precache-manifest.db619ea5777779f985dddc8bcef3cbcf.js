daa08a90133e2c22eefbe7fe2d2a2173a9796e27
vedant <mehtavedant7498@gmail.com>
2020-09-08 01:19:20 +0530

    first commit

---
+++ src/build/precache-manifest.db619ea5777779f985dddc8bcef3cbcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17aec279381b86c8d55c90de1d313379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f5ae73d522a78126dfb",
+    "url": "/static/css/main.bc587576.chunk.css"
+  },
+  {
+    "revision": "040b5dc5d6412627e544",
+    "url": "/static/js/2.1e8ce2c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1e8ce2c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f5ae73d522a78126dfb",
+    "url": "/static/js/main.fa528d17.chunk.js"
+  },
+  {
+    "revision": "caf43f0a14314da69f12",
+    "url": "/static/js/runtime-main.226f7c93.js"
+  }
+]);
